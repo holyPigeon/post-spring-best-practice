78ff53d79d1fe48f462dede8c93f5732203211fb
정재우 <awfjol2008@incom.land>
2026-05-07 13:24:32 +0900

    refactor(infrastructure): JPA Auditing 설정을 전용 클래스로 분리

--- src/main/java/com/example/springbestpractice/SpringBestPracticeApplication.java
+++ src/main/java/com/example/springbestpractice/SpringBestPracticeApplication.java
@@ -2,10 +2,8 @@ package com.example.springbestpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class SpringBestPracticeApplication {
 
     public static void main(String[] args) {
